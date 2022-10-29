1e1a0cbc38571048cffbb5c7755b81a19ac6b349
maximShamigovKrutoiChel <hui1337228.nagibator>
2022-10-29 20:44:27 +0300

    added bll

---
+++ MeetupAPI/Meetup.BLL/DI/BusinessLogicRegister.cs
@@ -0,0 +1,20 @@
+﻿using Meetup.BLL.Interfaces;
+using Meetup.BLL.Services;
+using Meetup.DAL.DI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Meetup.BLL.DI
+{
+    public static class BusinessLogicRegister
+    {
+        public static void AddBusinessLogic(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IUserServices, UserServices>();
+
+            services.AddScoped<IEventServices, EventServices>();
+
+            services.AddDataAccess(configuration);
+        }
+    }
+}
---
+++ MeetupAPI/Meetup.BLL/Interfaces/IServices.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meetup.BLL.Interfaces
+{
+    public interface IServices<TModel>
+    {
+        Task<IEnumerable<TModel>> GetAll(CancellationToken ct);
+
+        Task<TModel> GetById(int id, CancellationToken ct);
+
+        Task<TModel> Update(TModel item, CancellationToken ct);
+
+        Task<TModel> Create(TModel item, CancellationToken ct);
+
+        Task DeleteById(int id, CancellationToken ct);
+    }
+}
